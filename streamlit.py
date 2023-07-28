77ea77aefcff528e617b5f2272f6e370f1b59639
Aanya Tripathi <99536167+aanyatr@users.noreply.github.com>
2023-07-28 12:00:14 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -59,7 +59,7 @@ with st.container():
  with col3: # calling the animation question
   st_lottie(
     lottie_note,
-    speed=1,
+    speed=0.5,
     reverse=False,
     loop=True,
     quality="high", # medium ; high
